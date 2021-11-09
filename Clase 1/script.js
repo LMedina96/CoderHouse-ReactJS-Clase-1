d4ee7c1498afe8e4626ec7875cdad3fffa3f73ed
Lucas Medina <lucasgmedina96@gmail.com>
2021-11-09 16:31:38 -0300

    Form sin validación ni CSS

---
+++ Clase 1/script.js
@@ -0,0 +1,29 @@
+var cleanForm = () =>{
+    document.getElementById("idName").value = "";
+    document.getElementById("idContact").value = "";
+    document.getElementById("idComentary").value = "";
+}
+
+var sendCommentary = () =>{
+
+    let name = document.getElementById("idName").value;
+    let email = document.getElementById("idContact").value;
+    let commentary = document.getElementById("idComentary").value;
+
+    if (name != '' && email != '' && commentary != ''){
+        var newDiv = document.createElement("div");
+        newDiv.innerHTML = 
+        `<fieldset class='forum'>
+            <p>Usuario: ${name}</p>
+            <p>Email: ${email}</p>
+            <p>Mensaje: </p>
+            <p>${commentary}</p>
+        </fieldset>`
+    
+        document.body.appendChild(newDiv);
+    
+        cleanForm();
+    }else{
+        alert("Por favor llena todos los campos");
+    }
+}
